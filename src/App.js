31d53f9baea6b39090a9fefe84fdade249206128
George de Pinho <georgedipinho@hotmail.com>
2022-06-08 00:48:22 -0300

    consumindo API

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {StylesProvider} from '@material-ui/styles'
 import { CssBaseline} from '@material-ui/core'
 import GlobalStyle from './commons/styles/global-style'
+import Main from './containers/Main'
 
 
 function App() {
@@ -9,9 +10,7 @@ function App() {
     <StylesProvider injectFirst>
       <CssBaseline/>
       <GlobalStyle/>
-      <div>
-        Teste
-      </div>
+      <Main/>
     </StylesProvider>
   );
 }
